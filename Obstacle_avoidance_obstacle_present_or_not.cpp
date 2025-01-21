2ef4c2408b10e963ab9ea6b0c9a94b0fa78d8e5b
Yui-beep <yuilomlpaqt@gmail.com>
2025-01-21 10:25:11 +0800

    Create Obstacle_avoidance_obstacle_present_or_not.cpp
    tighs

---
+++ Obstacle_avoidance_obstacle_present_or_not.cpp
@@ -0,0 +1,17 @@
+const int SENSOR_PIN = 8;
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // initialize the Arduino's pin as aninput
+  pinMode(SENSOR_PIN, INPUT);
+}
+
+void loop() {
+  int state = digitalRead(SENSOR_PIN);
+  if (state == LOW)
+    Serial.println("The obstacle is present");
+  else
+    Serial.println("The obstacle is NOT present");
+  delay(100);
+}
